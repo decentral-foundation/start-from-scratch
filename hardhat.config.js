b0cd7d894ffe7ca432ea7ae8f388e5de394a4098
decentral-foundation <cryptography.lq@gmail.com>
2024-08-26 00:48:11 +0000

    env configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const { INFURA_API_KEY, PRIVATE_KEY } = process.env;
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.7.6",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
