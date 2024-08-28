628e8ba65aea05651bad0f3fe1b7984c7d97cf34
cco <cco@lingqingmeng.com>
2024-08-28 08:02:56 +0700

    add lucia sdk

--- frontend/src/components/MetaMaskLogin.tsx
+++ frontend/src/components/MetaMaskLogin.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
+import LuciaSDK from 'luciasdk-t3';
+
+const my_api_key:string=process.env.REACT_APP_LUCIASDK_T3_API_KEY as string;
+
 // import { ethers } from 'ethers';
+LuciaSDK.init({
+  clientId: "",
+  baseURL: "https://staging.api.clickinsights.xyz/fk",
+  api_key: my_api_key
+});
+
 
 const MetaMaskLogin: React.FC = () => {
   const [account, setAccount] = useState<string | null>(null);
@@ -10,6 +20,7 @@ const MetaMaskLogin: React.FC = () => {
         // Request account access
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         setAccount(accounts[0]);
+        LuciaSDK.sendWalletInfo(accounts[0],11155111)
       } catch (error) {
         console.error('User rejected the request.');
       }
