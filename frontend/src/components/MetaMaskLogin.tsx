7adf84cb50ba390040fd47d532602b7e5a22b97f
cco <cco@lingqingmeng.com>
2024-08-28 10:10:07 +0700

    need to figure out dotenv

--- frontend/src/components/MetaMaskLogin.tsx
+++ frontend/src/components/MetaMaskLogin.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import LuciaSDK from 'luciasdk-t3';
 
-const my_api_key:string=process.env.REACT_APP_LUCIASDK_T3_API_KEY as string;
+// const my_api_key:string=process.env.REACT_APP_LUCIASDK_T3_API_KEY as string;
+// console.log("my_api_key: ",my_api_key);
 
 // import { ethers } from 'ethers';
 LuciaSDK.init({
   clientId: "",
   baseURL: "https://staging.api.clickinsights.xyz/fk",
-  api_key: my_api_key
+  api_key: "048cd8fa-f9c4ea91-203c8378-16736034-f24ea457-1427a313-eb386d90-8575619f"
 });
+
+LuciaSDK.pageView("homepage");
 
 
 const MetaMaskLogin: React.FC = () => {
@@ -20,6 +23,7 @@ const MetaMaskLogin: React.FC = () => {
         // Request account access
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         setAccount(accounts[0]);
+        console.log("accounts[0]: ",accounts[0]);
         LuciaSDK.sendWalletInfo(accounts[0],11155111)
       } catch (error) {
         console.error('User rejected the request.');
