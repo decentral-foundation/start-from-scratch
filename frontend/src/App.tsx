d9ec7624feb94a415e0cf589705d2696d6b72a19
Ling Qing Meng <lingqingmeng@Lings-MacBook-Pro.local>
2024-11-05 15:12:11 -0500

    frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,32 @@ import MetaMaskLogin from './components/MetaMaskLogin';
 import TradingInterface from './components/TradingInterface';
 import WalletInteraction from './components/WalletInteraction';
 
+import { Core } from '@walletconnect/core';
+import { WalletKit } from '@reown/walletkit';
+
+
+console.log("WALLET_CONNECT_PROJECT_ID: ", import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID);
+const core = new Core({ 
+  projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || ''  
+})
+
+const onConnect = async (uri: string) => {
+  // call walletKit.core.pairing.pair( { uri: uri })
+  // with the uri received from the dapp in order to emit the
+  // `session_proposal` event
+  const result = await walletKit.core.pairing.pair({ uri })
+}
+
+const walletKit = await WalletKit.init({
+  core: core,
+  metadata: {
+    name: 'Lucia DEX',
+    description: 'Lucia DEX with WalletConnect Integration',
+    url: 'luciadex.com', //confirmed this is the domain name we own
+    icons: []
+  }
+})
+
 const App: React.FC = () => {
   const [account, setAccount] = useState<string | null>(null);
 
